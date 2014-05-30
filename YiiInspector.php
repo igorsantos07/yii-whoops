8598efc6d220082395d98938e4d595a4ced2a51b
Igor Santos <igorsantos07@gmail.com>
2014-05-30 14:38:08 -0300

    Correctly skipping useless exceptions in case of PHP errors and leaving stack untouched in case of real exceptions

--- YiiInspector.php
+++ YiiInspector.php
@@ -1,18 +1,29 @@
 <?php
-use Whoops\Exception\ErrorException;
 use Whoops\Exception\FrameCollection;
 
 class YiiInspector extends \Whoops\Exception\Inspector {
 
 	public function getFrames() {
-		//Removing all object wrappers and getting list of raw frames
 		$frames_obj = parent::getFrames()->getArray();
-		$frames = array_map(function($frame) { return $frame->getRawFrame(); }, $frames_obj);
 
-		//Skipping Whoops and Yii handlers
-		$frames = array_slice($frames, 4);
+		//If it's a PHP error we can skip all classes before it, as those are handlers' exceptions
+		if ($frames_obj[0]->getClass() == 'Whoops\Exception\ErrorException') {
 
-		return new FrameCollection($frames);
+			//Removing all object wrappers and getting list of raw frames
+			$frames = array_map(function($frame) { return $frame->getRawFrame(); }, $frames_obj);
+
+			$removable_keys = [0];
+			while(array_key_exists('class', next($frames)))
+				$removable_keys[] = key($frames);
+
+			foreach ($removable_keys as $key)
+				unset($frames[$key]);
+			$frames = array_merge($frames);
+
+			$frames_obj = new FrameCollection($frames);
+		}
+
+		return $frames_obj;
 	}
 
 }
